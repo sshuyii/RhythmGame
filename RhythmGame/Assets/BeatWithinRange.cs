3dd10e517e6f8f878e052ccb7d76553be3c0d78f
HaroldSama <122599Run>
2019-02-09 21:52:57 -0500

    One moe table + sign disappear at beat

--- RhythmGame/Assets/BeatWithinRange.cs
+++ RhythmGame/Assets/BeatWithinRange.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Boo.Lang;
 using UnityEngine;
 
 public class BeatWithinRange : MonoBehaviour {
 
 	public RhythmController RhythmController;
-
+	
 	// Use this for initialization
 	void Start () {
 		
+		
+
 	}
 	
 	// Update is called once per frame
@@ -21,6 +25,8 @@ public class BeatWithinRange : MonoBehaviour {
 		if (other.gameObject.CompareTag("Beat"))
 		{
 			RhythmController.chopEnabled += 1;
+			
+			RhythmController.ToBeDestroyed.Add(other.gameObject);//将进入范围的Beat标记添加到待摧毁列表（关联Script：RhythmController）
 		}
 	}
 	
@@ -29,6 +35,8 @@ public class BeatWithinRange : MonoBehaviour {
 		if (other.gameObject.CompareTag("Beat"))
 		{
 			RhythmController.chopEnabled -= 1;
+			
+			RhythmController.ToBeDestroyed.Remove(other.gameObject);//将离开范围的Beat标记从待摧毁列表中移除
 		}
 	}
 	
--- RhythmGame/Assets/Scripts/RhythmController.cs
+++ RhythmGame/Assets/Scripts/RhythmController.cs
@@ -7,6 +7,8 @@ public class RhythmController : MonoBehaviour
 	public int chopEnabled = 0;
 	public bool beatWithinRange = false;
 	public bool playerInPlace = false;
+
+	public List<GameObject> ToBeDestroyed;//创建一个待摧毁列表（关联Script：BeatWithinRange）
 	
 	private AudioSource beat;
 	
@@ -32,6 +34,14 @@ public class RhythmController : MonoBehaviour
 				print("space key was pressed");
 				beat.Play();
 				//chopEnabled += 1;
+                
+				//下面几行都是新加的
+				for (int i = 0; i < ToBeDestroyed.Count; i++)
+				{
+					Destroy(ToBeDestroyed[i]);
+					ToBeDestroyed.Remove(ToBeDestroyed[i]);
+					chopEnabled--;
+				}//摧毁待摧列表里所有的beat
 			}
 		}
 	}
