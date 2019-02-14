6a8c180132e0cda5324f89bd0410417057c8e0bb
ghaoyuxin <ghaoyuxin@gmail.com>
2019-02-14 15:23:36 -0500

    Added PlayerController script that support multiple players; added player1 prefab; updated input manager.

---
+++ RhythmGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Usage: Control players that uses Unity Input manager.
+//Intent: The controller can be used on multiple players.
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 6;
+    public int playerNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal" + playerNum) * speed;
+        //print("Got x input" + x);
+        float z = Input.GetAxisRaw("Vertical" + playerNum) * speed;
+        //print("Got y input");
+        playerRb.velocity = new Vector3(x, 0, z);
+    }
+}
