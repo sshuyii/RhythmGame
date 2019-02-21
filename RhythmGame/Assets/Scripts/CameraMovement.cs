6f94079808b09ec91f30dceb5e4f1366db4e5860
shuyi <mycalendula@gmail.com>
2019-02-20 22:26:41 -0500

    CameraMovement added.

---
+++ RhythmGame/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float CameraRotationLimit;
+    private int Timer = 100;
+
+    
+    private float CameraRotationYSum = 0;
+    private float CameraRotationY = 0;
+    private bool MoveLeft = true;
+    
+    private CameraStates _currentState;
+    public enum CameraStates {MoveLeft, MoveRight, StayStill}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotationYSum += CameraRotationY;
+        
+        print("CameraRotationYSum = " + CameraRotationYSum);
+        print("Timer = " + Timer);
+        
+        if (CameraRotationYSum > CameraRotationLimit)
+        {
+            Timer--;
+            if (Timer < 0)
+            {
+                _currentState = CameraStates.MoveLeft;
+                Timer = 200;
+            }
+            else
+            {
+                _currentState = CameraStates.StayStill;
+            }
+            
+        }
+        else if (CameraRotationYSum < -CameraRotationLimit)
+        {
+            Timer--;
+            if (Timer < 0)
+            {
+                _currentState = CameraStates.MoveRight;
+                Timer = 200;
+            }
+            else
+            {
+                _currentState = CameraStates.StayStill;
+            }
+           
+        }
+
+        if (_currentState == CameraStates.MoveLeft)
+        {
+            CameraRotationY = -Time.deltaTime;
+        }
+        else if(_currentState == CameraStates.MoveRight)
+        {
+            CameraRotationY = +Time.deltaTime;
+        }
+        else if (_currentState == CameraStates.StayStill)
+        {
+            CameraRotationY = 0;
+        }
+
+        transform.Rotate(0, CameraRotationY, 0, Space.World);
+    }
+    
+    
+}
