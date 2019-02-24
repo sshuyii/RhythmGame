eb9e64637a7b13d33f5aac99e68f14d0d7a13a3b
177sma <howerds4my@gmail.com>
2019-02-23 23:05:32 -0500

    Protype2 initial setup & player movement demo scene.

---
+++ RhythmGame Prototype2/RhythmGame Prototype2/Assets/Script/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Usage: Control players that uses Unity Input manager.
+//Intent: The controller can be used on multiple players.
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5;
+    public int playerNum;
+    public bool Keypressed;
+    public float Threshold = 0.5f;
+
+    public static int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal" + playerNum);
+        float z = Input.GetAxisRaw("Vertical" + playerNum);
+        
+        if (Mathf.Abs(x) >= Threshold && Keypressed == false)
+        {
+            playerRb.velocity = new Vector3(Mathf.Sign(x)*speed, 0, 0);
+            Invoke("Stop", 1f/speed);
+            Keypressed = true;
+        }
+        
+        if (Mathf.Abs(z) >= Threshold && Keypressed == false)
+        {
+            playerRb.velocity = new Vector3(0, 0, Mathf.Sign(z)*speed);
+            Invoke("Stop", 1f/speed);
+            Keypressed = true;
+        }
+
+
+        if (Mathf.Abs(x) <= Threshold && Mathf.Abs(z) <= Threshold)
+        {
+            Keypressed = false;
+        }
+  
+                        
+    }
+
+    void Stop()
+    {
+        playerRb.velocity = new Vector3(0, 0, 0);
+    }
+    
+}
