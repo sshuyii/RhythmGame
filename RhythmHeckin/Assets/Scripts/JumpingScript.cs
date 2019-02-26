c36fea5613d30527ed7dd60d32e071416612e2aa
ghaoyuxin <ghaoyuxin@gmail.com>
2019-02-25 22:53:01 -0500

    Uploaded Koreographer sample project RhythmHeckin

---
+++ RhythmHeckin/Assets/Scripts/JumpingScript.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+//make sure you're using the Koreo namespace
+using SonicBloom.Koreo;
+
+
+public class JumpingScript : MonoBehaviour {
+
+	
+	//We will use this when we implement the other characters
+	public enum Character {
+		B,
+		C,
+		D
+	};
+	public Character thisCharacter;
+
+	//audio clips for jumping and falling
+	public AudioClip jumpClip, fallClip;
+
+	//component references;
+	private AudioSource _audioSource;
+	private Animator _animator;
+
+	//an extra bool because the animator doesn't like when i check "isJumping";
+	private bool _BCanJump;
+
+	
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator>();
+		_audioSource = GetComponent<AudioSource>();
+		_BCanJump = false;
+		
+		//Register for the events being broadcasted from our koreography track
+		//In this case we called the event "BJumpEventID"
+		Koreographer.Instance.RegisterForEvents("BJumpEventID", BJumpWindow);
+		//We also need an event that signals when we are out of our jump window.
+		Koreographer.Instance.RegisterForEvents("BJumpEndEventID", BOutofJumpWindow);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			//check to see if we are within the window (and aren't already falling)
+			//if so, then jump!
+			if (_BCanJump) {
+				BJump();
+			}
+			
+			//if not, fall :-(
+			else {
+				BFall();
+			}
+			
+		}
+	}
+
+	//We trigger this in our animation clip so that B returns to idle after the jump/fall is complete
+	public void ReturnToIdle() {
+		_animator.SetBool("isJumping", false);
+		_animator.SetBool("isFalling", false);
+	}
+
+	
+	//Koreographer is going to call BJumpWindow across the span that we set
+	//in the Koreography track
+	
+	//We listen for that event, and set _BCanJump to true so that the player input
+	//is recognized across that event
+	public void BJumpWindow(KoreographyEvent koreographyEvent) {
+		_BCanJump = true;
+	}
+
+	public void BOutofJumpWindow(KoreographyEvent koreographyEvent) {
+		_BCanJump = false;
+	}
+
+	public void BFall() {
+		_animator.SetTrigger("Fall");
+		_animator.SetBool("isFalling", true);
+		_audioSource.clip = fallClip;
+		_audioSource.Play();
+	}
+
+	public void BJump() {
+		_animator.SetTrigger("Jump");
+		_animator.SetBool("isJumping", true);
+		_BCanJump = false;
+		_audioSource.clip = jumpClip;
+		_audioSource.Play();
+	}
+
+}
---
+++ RhythmHeckin/Assets/Scripts/SlapperSprite.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class SlapperSprite : MonoBehaviour {
+
+	public Sprite neutralSprite, slappedSprite;
+	//we want the left and right to be controlled by different keys
+	public enum WhichSlapper {left, right};
+	public WhichSlapper whichSlapper;
+	
+    //reference to the SpriteRenderer on this GameObject
+    private SpriteRenderer spriteRenderer;
+    
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		Koreographer.Instance.RegisterForEvents("SlapEventID", PlaySlapSound);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug Only
+		
+	}
+
+	public void SuccessfulSlap() 
+	{
+		StopAllCoroutines();
+		spriteRenderer.sprite = slappedSprite;
+		StartCoroutine(ReturnToNeutral(0.5f));
+	}
+
+	IEnumerator ReturnToNeutral(float returnTime) 
+	{
+		yield return new WaitForSeconds(returnTime);
+		spriteRenderer.sprite = neutralSprite;
+
+	}
+
+	public void PlaySlapSound(KoreographyEvent koreographyEvent) 
+	{
+		Debug.Log("should slap here");
+		if (Input.GetKeyDown(KeyCode.S) && whichSlapper == WhichSlapper.left) 
+		{
+			//activate left slapper
+			SuccessfulSlap();
+		}
+		else if (Input.GetKeyDown(KeyCode.L) && whichSlapper == WhichSlapper.right) 
+		{
+			//activate right slapper
+			SuccessfulSlap();
+		}
+		
+	}
+}
