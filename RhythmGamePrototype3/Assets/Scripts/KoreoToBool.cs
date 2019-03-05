8c926b5e2699d7c0c41b5b4998adb2066ebbe296
177sma <howerds4my@gmail.com>
2019-03-05 01:39:04 -0500

    Koreagrapher setup and animation on beats.

---
+++ RhythmGamePrototype3/Assets/Scripts/KoreoToBool.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class KoreoToBool : MonoBehaviour
+{
+    public static bool NewspaperEnable;
+    public static bool GlassEnable;
+    public static bool ChoppingEnable;
+
+
+    //public static KoreoToBool instance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Koreographer.Instance.RegisterForEvents("NewspaperEventID", NewspaperStart);
+        Koreographer.Instance.RegisterForEvents("NewspaperEndEventID", NewspaperEnd);
+        Koreographer.Instance.RegisterForEvents("GlassEventID", GlassStart);
+        Koreographer.Instance.RegisterForEvents("GlassEndEventID", GlassEnd);
+        Koreographer.Instance.RegisterForEvents("ChoppingEventID", ChoppingStart);
+        Koreographer.Instance.RegisterForEvents("ChoppingEndEventID", ChoppingEnd);
+    }
+    
+    void NewspaperStart(KoreographyEvent evt)
+    {
+            NewspaperEnable = true;
+            print("NewspaperStart");
+    }
+	
+    void NewspaperEnd(KoreographyEvent evt)
+    {
+            NewspaperEnable = false;
+            print("NewspaperEnd");
+    }
+
+    void GlassStart(KoreographyEvent evt)
+    {
+            GlassEnable = true;
+            print("GlassStart");
+    }
+	
+    void GlassEnd(KoreographyEvent evt)
+    {
+            GlassEnable = false;
+            print("GlassEnd");
+    }
+
+    void ChoppingStart(KoreographyEvent evt)
+    {
+            ChoppingEnable = true;
+            print("ChoppingStart");
+    }
+	
+    void ChoppingEnd(KoreographyEvent evt)
+    {
+            ChoppingEnable = false;
+            print("ChoppingEnd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RhythmGamePrototype3/Assets/Scripts/PayloadTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class PayloadTest : MonoBehaviour
+{
+    private AudioSource _audio;
+    public string EventID;
+    public float minScale = 0f;
+    public float maxScale = 1.2f;
+    private Vector3 interim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Koreographer.Instance.RegisterForEvents("TestTrack", TestPayload);
+        Koreographer.Instance.RegisterForEventsWithTime(EventID,TestOffset);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+/*    void TestPayload(KoreographyEvent evt)
+    {
+        print(evt.GetIntValue() + " " + Time.time);
+    }*/
+
+    void TestOffset(KoreographyEvent evt, int sampleTime, int sampleDelta, DeltaSlice deltaSlice)
+    {
+        /*print("OffSet " + Time.time);
+        print(sampleTime);
+        print(sampleDelta);*/
+        if (evt.HasCurvePayload())
+        {
+            //print("Scale Start");
+            // Get the value of the curve at the current audio position.  This will be a
+            //  value between [0, 1] and will be used, below, to interpolate between
+            //  minScale and maxScale.
+            float curveValue = Mathf.Floor(evt.GetValueOfCurveAtTime(sampleTime) * 5) / 5;
+            print(curveValue);
+
+            if (curveValue == 0)
+            {
+                transform.position = new Vector3(Random.Range(0 , 5), 0, Random.Range(-5 , 0));
+            }
+            interim = Vector3.one * Mathf.Lerp(minScale, maxScale, curveValue);
+            interim.y = 1.2f;
+            transform.localScale = interim;
+        }
+    }
+}
