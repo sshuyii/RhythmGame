bc96becd54cd718716a4186aaf27b91bc5f09fd8
177sma <howerds4my@gmail.com>
2019-03-05 16:29:16 -0500

    Beat Window added

--- RhythmGamePrototype3/Assets/Scripts/PlayerController.cs
+++ RhythmGamePrototype3/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SonicBloom.Koreo;
 
 //Usage: Control players that uses Unity Input manager.
 //Intent: The controller can be used on multiple players.
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody playerRb;
-    private Vector3 originalScale;
+    [Header("Player Setting")]
     public float speed = 6;
     public int playerNum;
     public KeyCode interaction;
@@ -16,18 +16,43 @@ public class PlayerController : MonoBehaviour
     [Header("Anime")]
     public float ShrinkDepth;
     public float RecoverRate;
+    public Material Normal;
+    public Material Perfect;
+    public Material Miss;
+
+    [Header("Koreo")] 
+    public string EventIDOpen;
+    public string EventIDClose;
+    
+    private Rigidbody playerRb;
+    private Vector3 originalScale;
+    private bool beatable;
+    private MeshRenderer rd;
 
     // Start is called before the first frame update
     void Start()
     {
+        Koreographer.Instance.RegisterForEvents(EventIDOpen,BeatReady);
+        Koreographer.Instance.RegisterForEvents(EventIDClose,BeatExpire);
+        
         playerRb = GetComponent<Rigidbody>();
         originalScale = transform.localScale;
+        rd = GetComponent<MeshRenderer>();
     }
 
+    void BeatReady(KoreographyEvent evt)
+    {
+        beatable = true;
+    }
+
+    void BeatExpire(KoreographyEvent evt)
+    {
+        beatable = false;
+    }
+    
     // Update is called once per frame
     void Update()
-    {
-                
+    {         
         transform.rotation = new Quaternion (0,  0, 0, 0);
 
         float x = Input.GetAxisRaw("Horizontal" + playerNum) * speed;
@@ -36,14 +61,23 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(interaction))
         {
-            transform.localScale -= new Vector3(0, originalScale.y * ShrinkDepth, 0);
-            Invoke("Recover", RecoverRate);
-        }
-        
+            if (beatable)
+            {
+               transform.localScale -= new Vector3(0, originalScale.y * ShrinkDepth, 0);
+               rd.material = Perfect;
+            }
+            else
+            {
+               rd.material = Miss;
+            }
+            
+            Invoke("Recover", RecoverRate); 
+        }    
     }
     
     void Recover()
     {
         transform.localScale = originalScale;
-    }
+        rd.material = Normal;
+    }    
 }
