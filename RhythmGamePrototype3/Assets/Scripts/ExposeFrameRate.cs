ab3a8d9302fa8aa4378680770ed61bd4533f6463
ghaoyuxin <ghaoyuxin@gmail.com>
2019-05-07 17:35:34 -0400

    cleaned up Koreography track assets by remaking them. Added frame rate script

---
+++ RhythmGamePrototype3/Assets/Scripts/ExposeFrameRate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ExposeFrameRate : MonoBehaviour
+{
+    public Text frameRate;
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameRate.text = (1f/Time.deltaTime).ToString();
+    }
+}
