67cb256adcca28e3d3444493700f5287b7eaf4fc
177sma <howerds4my@gmail.com>
2019-05-08 23:56:57 -0400

    Botton animation

---
+++ RhythmGamePrototype3/Assets/Scripts/Tutorial/ActiveBound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveBound : MonoBehaviour
+{
+    public GameObject parent;
+
+    //public bool active;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.SetActive(parent.activeInHierarchy);
+    }
+}
