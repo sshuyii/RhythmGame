39cc5cc716c4acffca1eb85ef67e4f88f21dd2ef
177sma <howerds4my@gmail.com>
2019-05-15 17:34:53 -0400

    Name correction

--- RhythmGamePrototype3/Assets/Scripts/Menu/ClockRotation.cs
+++ RhythmGamePrototype3/Assets/Scripts/Menu/ClockRotation.cs
@@ -14,6 +14,6 @@ public class ClockRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,speed);
+        transform.Rotate(0,0,speed * Time.deltaTime);
     }
 }
