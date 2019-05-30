3584506c123b9cbe055e27cb0c280d47a80066ae
ghaoyuxin <ghaoyuxin@gmail.com>
2019-05-30 03:49:51 -0400

    added script to keep 16:9 aspect ratio, changed tutorial UI....目前一团乱麻，runy 辛苦你了

---
+++ RhythmGamePrototype3/Assets/Scripts/KeepRatio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepRatio : MonoBehaviour
+{
+
+    public Camera LetterboxBgCamera;
+    public Camera MainCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        CalculateMainCameraDimensions();
+    }
+
+
+    public void CalculateMainCameraDimensions()
+    {
+        float sixteenBy9 = 16f / 9f;
+
+        if (LetterboxBgCamera.aspect < sixteenBy9)
+        {
+            MainCamera.rect = new Rect(0f, (1.0f - LetterboxBgCamera.aspect / sixteenBy9) / 2.0f, 1.0f, LetterboxBgCamera.aspect / sixteenBy9);
+
+        }
+        else if (LetterboxBgCamera.aspect > sixteenBy9)
+        {
+            MainCamera.rect = new Rect((1.0f - sixteenBy9 / LetterboxBgCamera.aspect) / 2.0f, 0, sixteenBy9 / LetterboxBgCamera.aspect, 1.0f);
+        }
+
+    }
+}
